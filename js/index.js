1d767dd1e6aa05dc629fece8527fd90730c0c28b
AhmedYLasheen <88255865+AhmedYLasheen@users.noreply.github.com>
2021-12-19 13:54:12 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -89,22 +89,3 @@ document.addEventListener("keydown",function(eventInfo)
 });
 
 
-
-
-
-
-
-// var imgs =document.querySelectorAll(".item img");
-// var lightBoxContainer =document.getElementById("lightBoxContainer");
-// var lightBoxItem =document.getElementById("lightBoxItem")
-
-// for (var i= 0; i < imgs.length ; i++)
-//  {
-//    imgs[i].addEventListener("click" ,function(eventInfo) {
-
-//     var imgSrc= eventInfo.target.getAttribute("src")
-//     lightBoxItem.style.backgroundImage="url("+imgSrc+")";
-//     lightBoxContainer.style.display ="flex";
-//    })
-    
-// }
